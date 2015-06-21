76e6978c6f47090f9059370f8d2c0c5e406a15ab
Alex Unwin <Alex.Unwin@infospace.com>
2015-06-20 20:49:02 -0700

    fuck it

---
+++ HackNight/Assets/Scripts/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+
+	private void Start () {
+		BeginGame();
+	}
+	
+	private void Update () {
+		if (Input.GetKeyDown(KeyCode.Space)) {
+			RestartGame();
+		}
+	}
+	
+	private void BeginGame () {
+	}
+	
+	private void RestartGame () {
+		BeginGame ();
+	}
+}
