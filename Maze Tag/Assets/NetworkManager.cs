632554f4270384bc93b14fd4da452ceb03423168
alex nishikawa <alex.d.nishikawa@gmail.com>
2015-06-20 22:35:08 -0700

    Network Manager with arrayed map

--- Maze Tag/Assets/NetworkManager.cs
+++ Maze Tag/Assets/NetworkManager.cs
@@ -2,14 +2,92 @@
 using System.Collections;
 
 public class NetworkManager : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
 	
+	private const string roomName = "RoomName";
+	private RoomInfo[] roomsList;
+	public GameObject Cube;
+	
+	void Start()
+	{
+		PhotonNetwork.ConnectUsingSettings("0.1");
 	}
 	
-	// Update is called once per frame
-	void Update () {
+	void OnJoinedRoom()
+	{
+		int[][] map = new int[][]{
+			//27 X 30
+			//Template new int[]{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+			//        1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7
+			new int[]{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},  //1
+			new int[]{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},  //2
+			new int[]{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},  //3
+			new int[]{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},  //4
+			new int[]{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},  //5
+			new int[]{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},  //6
+			new int[]{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},  //7
+			new int[]{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},  //8
+			new int[]{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},  //9
+			new int[]{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},  //10
+			new int[]{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},  //11
+			new int[]{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},  //12
+			new int[]{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},  //13
+			new int[]{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},  //14
+			new int[]{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},  //15
+			new int[]{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},  //16
+			new int[]{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},  //17
+			new int[]{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},  //18
+			new int[]{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},  //19
+			new int[]{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},  //20
+			new int[]{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},  //21
+			new int[]{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},  //22
+			new int[]{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},  //23
+			new int[]{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},  //24
+			new int[]{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},  //25
+			new int[]{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},  //26
+			new int[]{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},  //27
+			new int[]{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},  //28
+			new int[]{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},  //29
+			new int[]{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},  //30
+		};
+		
+		for (int r = 0; r < map.Length; r ++) {
+			for(int c = 0; c < map[0].Length; c++)
+			{
+				if(map[r][c] == 1)
+					PhotonNetwork.Instantiate("Cube", new Vector3(r * 1,0,c * 1), Quaternion.identity, 0);
+			}
+		}
+		
+		// Spawn player
+		//PhotonNetwork.Instantiate("Cube", Vector3.up, Quaternion.identity, 0);
+	}
 	
+	void OnGUI()
+	{
+		if (!PhotonNetwork.connected)
+		{
+			GUILayout.Label(PhotonNetwork.connectionStateDetailed.ToString());
+		}
+		else if (PhotonNetwork.room == null)
+		{
+			// Create Room
+			if (GUI.Button(new Rect(100, 100, 250, 100), "Start Server"))
+				PhotonNetwork.CreateRoom(roomName, true, true, 5);
+			
+			// Join Room
+			if (roomsList != null)
+			{
+				for (int i = 0; i < roomsList.Length; i++)
+				{
+					if (GUI.Button(new Rect(100, 250 + (110 * i), 250, 100), "Join " + roomsList[i].name))
+						PhotonNetwork.JoinRoom(roomsList[i].name);
+				}
+			}
+		}
+	}
+	
+	void OnReceivedRoomListUpdate()
+	{
+		roomsList = PhotonNetwork.GetRoomList();
 	}
 }
