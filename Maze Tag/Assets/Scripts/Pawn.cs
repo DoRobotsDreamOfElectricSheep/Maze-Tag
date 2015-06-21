a920b737cd2289e247bc61d468e3388bf602b94b
Alex Unwin <Alex.Unwin@infospace.com>
2015-06-21 00:28:11 -0700

    adding assests

---
+++ Maze Tag/Assets/Scripts/Pawn.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pawn : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var pos = transform.position;
+		pos += Camera.main.transform.forward * (float)2.0 * Time.deltaTime;
+		pos.y = 0;
+		transform.position = pos;
+	}
+}
